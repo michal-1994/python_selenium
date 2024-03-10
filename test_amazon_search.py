17f42d79eb4f292efc90d6827e01c2aaba95dc36
Michal <michalgrzegorczyk04@gmail.com>
2024-03-10 10:56:06 +0100

    add test for buty

--- test_amazon_search.py
+++ test_amazon_search.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-def test_amazon_search():
+def test_amazon_search_spodnie_meskie():
 	driver = webdriver.Chrome()
 	driver.implicitly_wait(10)
 	driver.get("https://www.amazon.pl/")
@@ -11,6 +11,21 @@ def test_amazon_search():
 	search.send_keys('spodnie męskie', Keys.ENTER)
 
 	expected_text = '"spodnie męskie"'
+	actual_text = driver.find_element(By.XPATH, '//span[@class="a-color-state a-text-bold"]').text
+
+	assert expected_text == actual_text, f"Error. Expected text {expected_text}, but actual text {actual_text}"
+
+	driver.quit()
+
+def test_amazon_search_buty():
+	driver = webdriver.Chrome()
+	driver.implicitly_wait(10)
+	driver.get("https://www.amazon.pl/")
+
+	search = driver.find_element(By.ID, 'twotabsearchtextbox')
+	search.send_keys('buty', Keys.ENTER)
+
+	expected_text = '"buty"'
 	actual_text = driver.find_element(By.XPATH, '//span[@class="a-color-state a-text-bold"]').text
 
 	assert expected_text == actual_text, f"Error. Expected text {expected_text}, but actual text {actual_text}"
