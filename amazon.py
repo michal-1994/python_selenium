6d5880eeb47b0122ec93f38f1d1b718744e7f61e
Michal <michalgrzegorczyk04@gmail.com>
2024-03-10 10:26:43 +0100

    amazon search bar test

---
+++ amazon.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome()
+driver.implicitly_wait(10)
+driver.get("https://www.amazon.pl/")
+
+search = driver.find_element(By.ID, 'twotabsearchtextbox')
+search.send_keys('spodnie męskie', Keys.ENTER)
+
+expected_text = '"spodnie męskie"'
+actual_text = driver.find_element(By.XPATH, '//span[@class="a-color-state a-text-bold"]').text
+
+assert expected_text == actual_text, f"Error. Expected text {expected_text}, but actual text {actual_text}"
+
+driver.quit()
