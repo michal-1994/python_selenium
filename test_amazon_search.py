b545f2ab04cfad7321f08b7574e349632bf60b29
Michal <michalgrzegorczyk04@gmail.com>
2024-03-10 11:38:28 +0100

    pytest and parametrize

--- test_amazon_search.py
+++ test_amazon_search.py
@@ -1,8 +1,10 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 class TestAmazon:
+	search_words = ('sukienka', 'buty', 'spodnie męskie')
 	driver = ''
 
 	def setup_method(self):
@@ -10,20 +12,12 @@ class TestAmazon:
 		self.driver.implicitly_wait(10)
 		self.driver.get("https://www.amazon.pl/")
 
-	def test_amazon_search_spodnie_meskie(self):
+	@pytest.mark.parametrize('search_query', search_words)
+	def test_amazon_search(self, search_query):
 		search = self.driver.find_element(By.ID, 'twotabsearchtextbox')
-		search.send_keys('spodnie męskie', Keys.ENTER)
+		search.send_keys(search_query, Keys.ENTER)
 
-		expected_text = '"spodnie męskie"'
-		actual_text = self.driver.find_element(By.XPATH, '//span[@class="a-color-state a-text-bold"]').text
-
-		assert expected_text == actual_text, f"Error. Expected text {expected_text}, but actual text {actual_text}"
-
-	def test_amazon_search_buty(self):
-		search = self.driver.find_element(By.ID, 'twotabsearchtextbox')
-		search.send_keys('buty', Keys.ENTER)
-
-		expected_text = '"buty"'
+		expected_text = f'\"{search_query}"'
 		actual_text = self.driver.find_element(By.XPATH, '//span[@class="a-color-state a-text-bold"]').text
 
 		assert expected_text == actual_text, f"Error. Expected text {expected_text}, but actual text {actual_text}"
