00af7ee7fb8d49f39031b6e072dff5b86ac65a95
Michal <michalgrzegorczyk04@gmail.com>
2024-03-11 10:55:02 +0100

    add test amazon empty cart

---
+++ test_amazon_empty_cart.py
@@ -0,0 +1,24 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+class TestAmazon:
+	driver = ''
+
+	def setup_method(self):
+		self.driver = webdriver.Chrome()
+		self.driver.implicitly_wait(10)
+		self.driver.get("https://www.amazon.pl/")
+
+	def test_amazon_empty_cart(self):
+		cart = self.driver.find_element(By.ID, 'nav-cart')
+		cart.click()
+
+		actual_text = self.driver.find_element(By.XPATH, '//div[@id="sc-active-cart"]//h2').text
+		expected_text = "Twój koszyk Amazon jest pusty"
+
+		assert expected_text == actual_text, f"Error. Expected text {expected_text}, but actual text {actual_text}"
+
+	def teardown_method(self):
+		self.driver.quit()
